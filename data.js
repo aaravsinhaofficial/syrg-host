4a45b57327dc1a7d415e7dfd54bba725cff5d5ba
Angad Kumar <angadkumar16ak@gmail.com>
2026-05-22 00:09:55 -0500

    Change LinkedIn links to Google for team members
    Updated LinkedIn links for team members to a Google link.

--- data.js
+++ data.js
@@ -31,7 +31,7 @@ window.SYRG_DATA = {
     {
       name: "Noah Abuja",
       role: "Technical Staff",
-      //linkedin: "https://www.linkedin.com/in/mrinal-yalageri-769967368/",
+      linkedin: "https://www.google.com",
     },
     {
       name: "Samanyu Pochanapeddi",
@@ -41,7 +41,7 @@ window.SYRG_DATA = {
     {
       name: "Aadi Dhagat",
       role: "Technical Staff",
-      //linkedin: "https://www.linkedin.com/in/mrinal-yalageri-769967368/",
+      linkedin: "https://www.google.com",
     },
   ],
 
